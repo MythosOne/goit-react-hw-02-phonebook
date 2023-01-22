78edc87324034220fe7088174441d2f046b10459
MythosOne <107143884+MythosOne@users.noreply.github.com>
2023-01-22 13:18:19 +0200

    goit-react-hw-02-phonebook-final-correct-interface

--- src/components/App.styled.js
+++ src/components/App.styled.js
@@ -86,13 +86,17 @@ export const ContainerFilter = styled.div`
 `;
 
 export const ContactsList = styled.ul`
-    list-style: none;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
     padding: 0;
 `;
 
 export const ContactsItem = styled.li`
+    width: 280px;
     display: flex;
-    justify-content: space-evenly;
+    justify-content: space-between;
     margin-bottom: 8px;
 `;
 
